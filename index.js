07550fbc9ef74b2f4def31cefe58b7189e55dc55
jjanmo <jjanmo07@hanmail.net>
2020-02-07 23:26:45 +0900

    Add middlewares such as helmet, morgan, body-parser, cookie-parser

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+
+
 const app = express();
 const port = 3000;
 
@@ -6,7 +12,15 @@ const handleListening = () => console.log(`Listening on http://localhost:${port}
 
 const handleHome = (req, res) => res.send('Welcome Home!!');
 
-const handleProfile = (req, res) => res.send('Hi My name is jjanmo');
+const handleProfile = (req, res) => res.send('Hi My name is CMY');
+
+//use middleware : morgan / body-parser /cookie-parser / helmet 
+
+app.use(morgan('dev')); //morgan is logger middleware
+app.use(helmet()); //helmet is help secure my express app
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get('/', handleHome);
 
