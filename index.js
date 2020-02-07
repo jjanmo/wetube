b79ed8ad35ecf6788ddc14fcb83f63497efc028e
jjanmo <jjanmo07@hanmail.net>
2020-02-07 21:25:29 +0900

    Make first home route in index.js

--- index.js
+++ index.js
@@ -6,4 +6,14 @@ function handleListening() {
     console.log(`Listening on http://localhost:${port}`);
 }
 
+function handleHome(req, res) {
+    console.log(req);
+    res.send('Hello This is home!');
+}
+
+app.get('/', handleHome);
+
+
 app.listen(port, handleListening);
+
+
