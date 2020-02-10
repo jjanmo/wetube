1b364ee59c8b72c95f98a0c2494daddeff256eb0
jjanmo <44132977+jjanmo@users.noreply.github.com>
2020-02-11 00:23:36 +0900

    Update pug files to add template variables

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
-export const join = (req, res) => res.render('join');
-export const login = (req, res) => res.render('login');
-export const logout = (req, res) => res.render('logout');
-export const users = (req, res) => res.render('users');
-export const userDetail = (req, res) => res.render('userDetail');
-export const editProfile = (req, res) => res.render('editProfile');
-export const changePassword = (req, res) => res.render('changePassword');
+export const join = (req, res) => res.render('join', { pageName: 'JOIN' });
+export const login = (req, res) => res.render('login', { pageName: 'LOGIN' });
+export const logout = (req, res) => res.render('logout', { pageName: 'LOGOUT' });
+export const users = (req, res) => res.render('users', { pageName: 'USERS' });
+export const userDetail = (req, res) => res.render('userDetail', { pageName: 'USER NAME' });
+export const editProfile = (req, res) => res.render('editProfile', { pageName: 'EDIT PROFILE' });
+export const changePassword = (req, res) => res.render('changePassword', { pageName: 'CHANGE PASSWORD' });
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,7 @@
-export const home = (req, res) => res.render('home');
-export const search = (req, res) => res.render('search');
-export const videos = (req, res) => res.render('video');
-export const upload = (req, res) => res.render('upload');
-export const videoDetail = (req, res) => res.render('videoDetail');
-export const editVideo = (req, res) => res.render('editVideo');
-export const deleteVideo = (req, res) => res.render('deleteVideo');
+export const home = (req, res) => res.render('home', { pageName: 'HOME' });
+export const search = (req, res) => res.render('search', { pageName: 'SEARCH' });
+export const videos = (req, res) => res.render('video', { pageName: 'VIDEO' });
+export const upload = (req, res) => res.render('upload', { pageName: 'UPLOAD' });
+export const videoDetail = (req, res) => res.render('videoDetail', { pageName: 'VIDEO NAME' });
+export const editVideo = (req, res) => res.render('editVideo', { pageName: 'EDIT VIDEO' });
+export const deleteVideo = (req, res) => res.render('deleteVideo', { pageName: 'DELETE VIDEO' });
