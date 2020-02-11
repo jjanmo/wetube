d2633ed3dcee766948b302af855489281096a933
jjanmo <jjanmo07@hanmail.net>
2020-02-11 14:48:44 +0900

    Make login,join and search page

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,5 +1,12 @@
 export const home = (req, res) => res.render('home', { pageName: 'HOME' });
-export const search = (req, res) => res.render('search', { pageName: 'SEARCH' });
+
+export const search = (req, res) => {
+    // const term = req.query.term;
+    //-> 아래처럼 사용하는 것이 더 섹쉬함! ES6
+    const { query: { term } } = req;
+    res.render('search', { pageName: 'SEARCH', term }); //term : term => term으로 한 번에 사용가능
+}
+
 export const videos = (req, res) => res.render('video', { pageName: 'VIDEO' });
 export const upload = (req, res) => res.render('upload', { pageName: 'UPLOAD' });
 export const videoDetail = (req, res) => res.render('videoDetail', { pageName: 'VIDEO NAME' });
