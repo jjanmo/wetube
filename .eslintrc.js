c64df7677d7ca999a1fcf56247d9de9d6562b4f6
jjanmo <44132977+jjanmo@users.noreply.github.com>
2020-02-19 00:48:16 +0900

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    extends: 'airbnb-base',
+    env: {
+        browser: true,
+        es6: true
+    },
+    extends: 'eslint:recommended',
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly'
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module'
+    },
+    rules: {}
+};
