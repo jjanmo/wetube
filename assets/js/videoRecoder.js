bb77bc99bc68dea822b62cf7fbd806f37115810e
jjanmo <jjanmo07@hanmail.net>
2020-03-09 00:48:16 +0900

    Fix error in recording part

--- assets/js/videoRecoder.js
+++ assets/js/videoRecoder.js
@@ -7,7 +7,7 @@ let videoRecorder;
 
 const handleVideoData = e => {
     console.log(e);
-    const { data: videoFile } = event;
+    const { data: videoFile } = event; //data is blob
     const link = document.createElement('a');
     link.href = URL.createObjectURL(videoFile);
     link.download = 'recorded.webm';
@@ -17,8 +17,10 @@ const handleVideoData = e => {
 
 const startRecording = () => {
     videoRecorder = new MediaRecorder(streamObject);
+    console.log(videoRecorder);
     videoRecorder.start();
     videoRecorder.addEventListener('dataavailable', handleVideoData);
+    //-> 이벤트가 끝날 때(녹화가 끝날때) 녹화한 내용을 얻을 수 있음
     recorderBtn.addEventListener('click', stopRecording);
 }
 
@@ -31,13 +33,15 @@ const stopRecording = () => {
 
 const getVideo = async () => {
     try {
+        //stream을 얻는다
         const stream = await navigator.mediaDevices.getUserMedia({
             audio: true,
             video: { width: 1280, height: 720 }
         });
+        //stream을 video(여기선 videoPreview)에 넣는다
         if ('srcObject' in videoPreview) videoPreview.srcObject = stream;
         else videoPreview.src = window.URL.createObjectURL(stream);
-        // videoPreview.muted = true;
+        videoPreview.muted = true;
         videoPreview.play();
         recorderBtn.innerHTML = 'STOP RECORDING';
         streamObject = stream;
@@ -46,7 +50,7 @@ const getVideo = async () => {
         console.log(error);
         recorderBtn.innerHTML = 'Can NOT record'
     } finally {
-        recorderBtn.removeEventListener('click', startRecording);
+        recorderBtn.removeEventListener('click', getVideo);
     }
 }
 
