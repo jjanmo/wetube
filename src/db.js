a4e764debe0d3d70c0acfb56f1b13b43009a4dd2
jjanmo <jjanmo07@hanmail.net>
2020-03-27 22:55:20 +0900

    Update db.js

--- src/db.js
+++ src/db.js
@@ -8,8 +8,7 @@ dotenv.config();
 
 mongoose.connect(process.env.PRODUCTION ? process.env.MONGO_URL_PROD : process.env.MONGO_URL, {
     useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
+    useFindAndModify: false
 });
 
 // var MongoClient = require('mongodb').MongoClient;
